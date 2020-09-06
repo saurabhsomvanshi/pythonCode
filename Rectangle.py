9679eaeda264dc8f59541859609d4a54da970dd7
saurabhsomvanshi <mantra2907@gmail.com>
2020-09-07 00:06:20 +0530

    add new file * rectangle

---
+++ Rectangle.py
@@ -0,0 +1,36 @@
+#Print * Rectangle
+'''
+*****
+*****
+*****
+*****
+*****
+
+'''
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end="")  #  By default line ended with new line, here we define, nothing to be added at the end of line
+
+    print()
+
+print(" 22222 +++++++ =======================================")
+#Print * Rectangle
+'''
+*****
+*   *
+*   *
+*   *
+*****
+'''
+
+for i in range(1,6):
+    for j in range(1,6):
+        if(i==1 or i==5):
+            print("*",end="")
+        else:
+            if(j==1 or j==5):
+                print("*", end="")
+            else:
+                print(" ",end="")
+
+    print()
